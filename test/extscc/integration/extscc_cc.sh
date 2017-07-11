e49e73fab8330deca1b89f6b1152098780190747
Aleksandar Likic <aleksandar.likic@securekey.com>
2017-07-11 14:21:38 -0400

    [DEV-2108] Fixed reading external SCC configuration
    Change-Id: I4524f19f385041df6c54f0cf73a4b8657da4279a
    Signed-off-by: Aleksandar Likic <aleksandar.likic@securekey.com>

--- test/extscc/integration/extscc_cc.sh
+++ test/extscc/integration/extscc_cc.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-export CORE_CHAINCODE_ID_NAME=snap1:1.0.0-rc1
+export CORE_CHAINCODE_ID_NAME=extscc:1.0.0-rc1
 export CORE_PEER_ADDRESS=peer0.org1.example.com:7051
 export CORE_CHAINCODE_LOGGING_LEVEL=debug
 export CORE_PEER_TLS_ENABLED=true
@@ -13,7 +13,7 @@ export CORE_PEER_TLS_SERVERHOSTOVERRIDE=peer0.org1.example.com
 export CORE_PEER_TLS_ROOTCERT_FILE=$GOPATH/src/github.com/hyperledger/fabric-sdk-go/test/fixtures/channel/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/cacerts/org1.example.com-cert.pem
 go run ../fixtures/src/github.com/extscc_cc/extscc_cc.go&
 
-export CORE_CHAINCODE_ID_NAME=snap1:1.0.0-rc1
+export CORE_CHAINCODE_ID_NAME=extscc:1.0.0-rc1
 export CORE_PEER_ADDRESS=peer0.org2.example.com:8051
 export CORE_CHAINCODE_LOGGING_LEVEL=debug
 export CORE_PEER_TLS_ENABLED=true
