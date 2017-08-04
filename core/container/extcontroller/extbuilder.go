72419333ff85bd740308fbfd019e27b5c5f373e1
Baha Shaaban <baha.shaaban@securekey.com>
2017-08-04 16:56:25 -0400

    Optimization of SCC Compilation - adding bin hash to prevent recompilation of EXT SCCs                                 - used peer.fileSystemPath + /chaincodes as base path for ext scc binaries and hash files
    Change-Id: I245ffc7b77eec431225f6315b285504c40419e57
    Signed-off-by: Baha Shaaban <baha.shaaban@securekey.com>

--- core/container/extcontroller/extbuilder.go
+++ core/container/extcontroller/extbuilder.go
@@ -16,7 +16,14 @@ import (
 	"strings"
 
 	"github.com/hyperledger/fabric/core/chaincode/platforms/util"
+	"github.com/hyperledger/fabric/core/config"
 	"github.com/hyperledger/fabric/core/container/dockercontroller"
+
+	"encoding/hex"
+
+	"io/ioutil"
+
+	"crypto/md5"
 
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
@@ -25,9 +32,18 @@ import (
 // source by using a Docker container, and then extracts the chaincode binary
 // so that it can be used for execution within a process or Docker container
 func GenerateExtBuild(cds *pb.ChaincodeDeploymentSpec, cert []byte) (io.Reader, error) {
-	curDir, err := os.Getwd()
-	if err != nil {
-		return nil, fmt.Errorf("Error getting current directory: %v", err)
+	curDir := filepath.Join(config.GetPath("peer.fileSystemPath"), "chaincodes")
+
+	if isBinExtCCbuilt(curDir, cds.ChaincodeSpec.ChaincodeId.Name) {
+		ccBinOutPath := filepath.Join(curDir, cds.ChaincodeSpec.ChaincodeId.Name)
+		f, err := os.Open(ccBinOutPath)
+
+		if err != nil {
+			return nil, err
+		}
+		f.Close()
+
+		return strings.NewReader(ccBinOutPath), nil
 	}
 
 	builder := func() (io.Reader, error) { return dockercontroller.GenerateDockerBuild(cds, cert) }
@@ -42,7 +58,7 @@ func GenerateExtBuild(cds *pb.ChaincodeDeploymentSpec, cert []byte) (io.Reader, 
 		return nil, fmt.Errorf("Error opening gzip reader for code package: %v", err)
 	}
 
-	binPkgOutputPath := filepath.Join(curDir, "/binpackage.tar")
+	binPkgOutputPath := filepath.Join(curDir, "binpackage.tar")
 
 	err = util.ExtractFileFromTar(gr, "binpackage.tar", binPkgOutputPath)
 	gr.Close()
@@ -55,7 +71,7 @@ func GenerateExtBuild(cds *pb.ChaincodeDeploymentSpec, cert []byte) (io.Reader, 
 		return nil, fmt.Errorf("Error opening binpackage.tar: %v", err)
 	}
 
-	ccBinOutPath := filepath.Join(curDir, "/", cds.ChaincodeSpec.ChaincodeId.Name)
+	ccBinOutPath := filepath.Join(curDir, cds.ChaincodeSpec.ChaincodeId.Name)
 
 	err = util.ExtractFileFromTar(binPkgTarFile, "./chaincode", ccBinOutPath)
 	binPkgTarFile.Close()
@@ -63,5 +79,89 @@ func GenerateExtBuild(cds *pb.ChaincodeDeploymentSpec, cert []byte) (io.Reader, 
 		return nil, fmt.Errorf("Error extracting chaincode binary from binpackage.tar: %v", err)
 	}
 
+	generateExtCCHashFile(curDir, cds.ChaincodeSpec.ChaincodeId.Name, ccBinOutPath)
+
 	return strings.NewReader(ccBinOutPath), nil
+}
+func generateExtCCHashFile(curDir string, ccName string, ccBinOutPath string) error {
+	d, err := ioutil.ReadFile(ccBinOutPath)
+	if err != nil {
+		extLogger.Warning("Bin file does not exist. Cannot generate hash file. Err: %s", err)
+		return err
+	}
+
+	name := computeHashFilenameForBin(ccName, d)
+	outputHashFilePath := filepath.Join(curDir, "hash", name)
+
+	//remove any existing hash prior to creating a new hash for this ext scc
+	err = deleteAllHashFiles(curDir, ccName)
+	if err != nil {
+		extLogger.Warningf("Failed to clean "+ccName+" hash files for binary build. Err: %s", err)
+		return err
+	}
+
+	var f *os.File = nil
+	defer f.Close()
+	f, err = os.Create(outputHashFilePath)
+	if err != nil {
+		extLogger.Warningf("Failed to generate hash file for binary build. Err: %s", err)
+		return err
+	}
+
+	return nil
+}
+func isBinExtCCbuilt(curDir string, ccName string) bool {
+	ccBinOutPath := filepath.Join(curDir, ccName)
+	// if bin file doesn't exist, then don't bother computing the hash, return false
+	if _, err := os.Stat(ccBinOutPath); os.IsNotExist(err) {
+		return false
+	}
+
+	d, err := ioutil.ReadFile(ccBinOutPath)
+	if err != nil {
+		extLogger.Warningf("Can't read Bin hash file. Either delete hash from to force a recompile or verify the hash is a valid file. Bin file: %s", ccBinOutPath)
+	}
+	name := computeHashFilenameForBin(ccName, d)
+	hashFound := false
+	if name != "" && len(name) > 0 {
+
+		if _, err := os.Stat(filepath.Join(curDir, "hash", name)); !os.IsNotExist(err) {
+			hashFound = true
+		}
+
+	}
+	return hashFound
+}
+func computeHashFilenameForBin(ccName string, data []byte) string {
+	h := md5.New()
+	h.Write(data)
+	name := ccName + hex.EncodeToString(h.Sum(nil))
+	return name
+}
+func deleteAllHashFiles(curDir string, ccName string) error {
+	dirname := filepath.Join(curDir, "hash")
+	d, err := os.Open(dirname)
+	if err != nil {
+		// try to create the hash directory if it doesn't exist
+		err = os.MkdirAll(dirname, os.ModePerm)
+		return err
+	}
+	defer d.Close()
+
+	files, err := d.Readdir(-1)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if file.Mode().IsRegular() {
+			if strings.HasPrefix(file.Name(), ccName) {
+				err = os.Remove(filepath.Join(dirname, file.Name()))
+				if err != nil {
+					return fmt.Errorf("Unable to delete hash file: %s. Error: %s", file.Name(), err)
+				}
+			}
+		}
+	}
+	return nil
 }
