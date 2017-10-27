dfd2568734902250613a9f2e2f74e2de900c3237
Firas Qutishat <firas.qutishat@securekey.com>
2017-10-27 11:15:10 -0400

    [DEV-3557] CI for testing the fabric-next
    Change-Id: I87a9f0a6bfd9d4004aab87106c56636171c86cfd
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

---
+++ scripts/pull_fabric.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright SecureKey Technologies Inc. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+set -e
+
+
+
+if [ -d $GOPATH/src/github.com/hyperledger/fabric ] 
+then mv $GOPATH/src/github.com/hyperledger/fabric $GOPATH/src/github.com/hyperledger/fabric-original
+fi
+
+mkdir -p $GOPATH/src/github.com/hyperledger/
+cd $GOPATH/src/github.com/hyperledger/
+git clone https://gerrit.hyperledger.org/r/fabric
+cd fabric
+git checkout 4f7a7c8d696e866d06780e14b10704614a68564b
+
+$GOPATH/src/github.com/hyperledger/fabric-next/scripts/fabric_cherry_picks.sh
+
+make clean
+DOCKER_DYNAMIC_LINK=true BASE_DOCKER_NS=d1vyank make docker
