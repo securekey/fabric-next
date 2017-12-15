990a71f9dcd4871716018ca759165ad5a69dce82
Divyank Katira <divyank.katira@securekey.com>
2017-12-15 07:24:56 +0530

    Use base-image from build
    Change-Id: I0ef71cee15ce1177507ef9f4257e980a3e65c262
    Signed-off-by: Divyank Katira <divyank.katira@securekey.com>

--- scripts/pull_fabric.sh
+++ scripts/pull_fabric.sh
@@ -7,20 +7,21 @@
 set -e
 
 declare -x FABRIC_BASE_OS_IMAGE=hyperledger/fabric-baseos
-# Using images from a developer's namespace until hyperledger releases images with Go1.9.2
-declare -x FABRIC_BASE_IMAGE=d1vyank/fabric-baseimage
+# Using images built from the tip of https://github.com/hyperledger/fabric-baseimage until hyperledger tags new images with Go1.9.2
+declare -x FABRIC_BASE_IMAGE=repo.onetap.ca:8443/next/hyperledger/fabric-baseimage
+declare -x BASE_VERSION=0.4.3-snapshot-168b4d5
 declare -x BASE_NAMESPACE=securekey
 # This must match the version of fabric that is being cherry-picked
-declare -x BASE_VERSION=0.4.2
+declare -x BASE_OUTPUT_VERSION=0.4.2
 declare -x ARCH=$(uname -m)
 
 # Build base images to enable dynamic build
-docker build -f ./images/fabric-baseos/Dockerfile --no-cache -t ${BASE_NAMESPACE}/fabric-baseos:${ARCH}-${BASE_VERSION} \
+docker build -f ./images/fabric-baseos/Dockerfile --no-cache -t ${BASE_NAMESPACE}/fabric-baseos:${ARCH}-${BASE_OUTPUT_VERSION} \
 --build-arg FABRIC_BASE_OS_IMAGE=${FABRIC_BASE_OS_IMAGE} \
 --build-arg ARCH=${ARCH} \
 --build-arg FABRIC_BASE_VERSION=${BASE_VERSION} .
 
-docker build -f ./images/fabric-baseimage/Dockerfile --no-cache -t ${BASE_NAMESPACE}/fabric-baseimage:${ARCH}-${BASE_VERSION} \
+docker build -f ./images/fabric-baseimage/Dockerfile --no-cache -t ${BASE_NAMESPACE}/fabric-baseimage:${ARCH}-${BASE_OUTPUT_VERSION} \
 --build-arg FABRIC_BASE_IMAGE=${FABRIC_BASE_IMAGE} \
 --build-arg ARCH=${ARCH} \
 --build-arg FABRIC_BASE_VERSION=${BASE_VERSION} .
