db97a37c0180f71e836bde4a696720a2a6a4d050
Baha Shaaban <baha.shaaban@securekey.com>
2018-06-08 17:40:26 -0400

    udpate hyperledger url & Fix merge conflict
    patch from https://jira.hyperledger.org/browse/FAB-10576
    is conflicting with Fabric 1.1 branch. The patch
    "0001-FAB-10576-Check-if-puller-got-valid-pvt-rwset.patch"
     fixes the conflict
    
    also updated the fabric_cherry_picks script to pull Fabric
    from the branch release-1.1 instead of from the tag v1.1.0
    
    so there's no need to cherry-pick release-1.1 changes in the
    script anymore.
    
    Change-Id: I9b3a173964f577a7c3ef81dcd81ec7641bf8ca10
    Signed-off-by: Baha Shaaban <baha.shaaban@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -20,7 +20,7 @@ cd $GOPATH/src/github.com/hyperledger/
 git clone https://gerrit.hyperledger.org/r/fabric
 cd fabric
 git config advice.detachedHead false
-git checkout v1.1.0
+git checkout release-1.1
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
@@ -59,10 +59,6 @@ git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/69/14769/7 && git
 #https://gerrit.hyperledger.org/r/#/c/14791/ - Open - [FAB-6717] - Implement Private Data Funcs in MockStub
 git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/91/14791/5 && git cherry-pick FETCH_HEAD
 
-#Deliver Service bug fix
-#https://gerrit.hyperledger.org/r/c/20239/ - Closed - [FAB-9389] peer deliver client crashes when it is started and stopped immediately after
-git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/39/20239/1 && git cherry-pick FETCH_HEAD
-
 # https://gerrit.hyperledger.org/r/c/22553/ - Open - Gossip deadlock issue
 git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/53/22553/1 && git cherry-pick FETCH_HEAD
 
@@ -90,9 +86,8 @@ git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/81/19281/6 && git
 
 ##TODO cherry pick service discovery##
 
-# https://jira.hyperledger.org/browse/FAB-10576 - Open - [FAB-10576] Prevent peer from crashing while processing pull request in absense of collection config
-git fetch https://gerrit.hyperledger.org:29418/fabric refs/changes/71/22871/1 && git cherry-pick FETCH_HEAD
 
-# latest 1.1 branch release commit including previous bug fixes that were not cherry picked before
-# https://jira.hyperledger.org/browse/FAB-10521 - Merged - [FAB-10521] Block-cutter should refetch config
-git fetch https://gerrit.hyperledger.org:29418/fabric refs/changes/55/22855/2 && git cherry-pick FETCH_HEAD
+# https://gerrit.hyperledger.org/r/c/22871/ - Merged -  [FAB-10576]: Check if puller got valid pvt rwset
+#git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/71/22871/3 && git cherry-pick FETCH_HEAD
+git am $MY_PATH/../patches/0001-FAB-10576-Check-if-puller-got-valid-pvt-rwset.patch
+
