ee9f1d708a17dde62034901f38284f590b04efcf
Sudesh Shetty <sudesh.shetty@securekey.com>
2018-06-12 11:45:06 -0400

    ep11 remote bccsp patch
    Change-Id: I86601210f86b9e29f4dd07d18475e7d4b439cf0b
    Signed-off-by: Sudesh Shetty <sudesh.shetty@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -6,15 +6,29 @@
 #
 set -e
 
+# NEEDED for GREP11 patch
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
 
 mkdir -p $GOPATH/src/github.com/hyperledger/
 cd $GOPATH/src/github.com/hyperledger/
 git clone https://gerrit.hyperledger.org/r/fabric
 cd fabric
 git config advice.detachedHead false
-git checkout d7656e2bff0a368cda09d9e2b190ed8702938219
+git checkout f9ab89e02aeea22621f2b3bdd94e7e3f9492e4e3
 
-#cd $GOPATH/src/github.com/hyperledger/fabric
+cd $GOPATH/src/github.com/hyperledger/fabric
 
-#git config user.name "jenkins"
-#git config user.email jenkins@jenkins.com
+git config user.name "jenkins"
+git config user.email jenkins@jenkins.com
+
+#apply patch for GREP11 (5151f212d3edd89fbabc12fbe702cecea0cb4b3a + local fixes)
+git am $MY_PATH/../patches/0001-GREP11-Remote-EP11-BCCSP.patch
+
+
