d5abe12d9f362b0a4a139fe0aed5cffd9cbdc926
Sudesh Shetty <sudesh.shetty@securekey.com>
2018-06-12 11:40:15 -0400

    Remote EP11 BCCSP
    This new provider connects directly to EP11 library. This library
    is much closer to the device driver for the IBM Crypto Cards.
    
    Change-Id: I1d8646edce441dd03af72263fd410df89dfbf553
    Signed-off-by: Volodymyr Paprotski <vpaprots@ca.ibm.com>

--- bccsp/factory/pkcs11.go
+++ bccsp/factory/pkcs11.go
@@ -1,26 +1,17 @@
 // +build pkcs11
 
 /*
-Copyright IBM Corp. 2017 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Copyright IBM Corp. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
 */
+
 package factory
 
 import (
 	"github.com/hyperledger/fabric/bccsp"
 	"github.com/hyperledger/fabric/bccsp/pkcs11"
 	"github.com/pkg/errors"
+	"github.com/hyperledger/fabric/bccsp/grep11"
 )
 
 // FactoryOpts holds configuration information used to initialize factory implementations
@@ -29,6 +20,7 @@ type FactoryOpts struct {
 	SwOpts       *SwOpts            `mapstructure:"SW,omitempty" json:"SW,omitempty" yaml:"SwOpts"`
 	PluginOpts   *PluginOpts        `mapstructure:"PLUGIN,omitempty" json:"PLUGIN,omitempty" yaml:"PluginOpts"`
 	Pkcs11Opts   *pkcs11.PKCS11Opts `mapstructure:"PKCS11,omitempty" json:"PKCS11,omitempty" yaml:"PKCS11"`
+	Grep11Opts   *grep11.GREP11Opts `mapstructure:"GREP11,omitempty" json:"GREP11,omitempty" yaml:"GREP11"`
 }
 
 // InitFactories must be called before using factory interfaces
@@ -87,6 +79,15 @@ func setFactories(config *FactoryOpts) error {
 		}
 	}
 
+	// GREP11-Based BCCSP
+	if config.Grep11Opts != nil {
+		f := &GREP11Factory{}
+		err := initBCCSP(f, config)
+		if err != nil {
+			factoriesInitError = errors.Errorf("Failed initializing GREP11.BCCSP %s\n[%s]", factoriesInitError, err)
+		}
+	}
+
 	var ok bool
 	defaultBCCSP, ok = bccspMap[config.ProviderName]
 	if !ok {
@@ -106,6 +107,8 @@ func GetBCCSPFromOpts(config *FactoryOpts) (bccsp.BCCSP, error) {
 		f = &PKCS11Factory{}
 	case "PLUGIN":
 		f = &PluginFactory{}
+	case "GREP11":
+		f = &GREP11Factory{}
 	default:
 		return nil, errors.Errorf("Could not find BCCSP, no '%s' provider", config.ProviderName)
 	}
