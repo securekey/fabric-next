92d9b8b390757159886c85550c3a947c11b6ebd5
Aleksandar Likic <aleksandar.likic@securekey.com>
2018-08-09 18:16:11 -0400

    Ledger storage metrics
    Change-Id: I8ef59ab51583eecd77df142c6b39161112766905
    Signed-off-by: Aleksandar Likic <aleksandar.likic@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -32,3 +32,5 @@ git config user.email jenkins@jenkins.com
 git am $MY_PATH/../patches/0001-GREP11-Remote-EP11-BCCSP.patch
 
 git am $MY_PATH/../patches/0001-FAB-11247-Add-configuration-to-create-_global_change.patch
+
+git am $MY_PATH/../patches/0001-Ledger-metrics.patch
