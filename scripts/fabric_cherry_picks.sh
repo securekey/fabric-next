5aecbcaed04eb545b72d4ce2941cf9a283b41926
Troy Ronda <troy.ronda@securekey.com>
2018-08-29 10:42:56 -0400

    RELEASE Fabric-next 1.2.0-0.1.6
    Change-Id: I4dbbc2c7874a4d4d416a117e09e364dbefa10cce
    Signed-off-by: Troy Ronda <troy.ronda@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -21,8 +21,8 @@ cd $GOPATH/src/github.com/hyperledger/
 git clone https://gerrit.hyperledger.org/r/fabric
 cd fabric
 git config advice.detachedHead false
-# Tip of release-1.2 (August 27, 2018)
-git checkout 644f38d078108bdb276b21810223a9388cb7ed1c
+# Tip of release-1.2 (August 29, 2018)
+git checkout a58896e4b1b5ed071d76e6244174aeac853a7ba1
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
@@ -30,7 +30,7 @@ git config user.name "jenkins"
 git config user.email jenkins@jenkins.com
 
 # Cherry pick [FAB-11777] Expose LevelDB configuration
-git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/29/25929/4 && git cherry-pick FETCH_HEAD
+#git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/29/25929/4 && git cherry-pick FETCH_HEAD
 
 #apply patch for GREP11 (5151f212d3edd89fbabc12fbe702cecea0cb4b3a + local fixes)
 git am $MY_PATH/../patches/0001-GREP11-Remote-EP11-BCCSP.patch
@@ -41,7 +41,7 @@ git am $MY_PATH/../patches/0001-FAB-11247-Add-configuration-to-create-_global_ch
 # [FAB-8622] Reduce exclusive lock duration during commit
 #git am $MY_PATH/../patches/0001-FAB-8622-Red.-exclusive-lock-dur.-during-commit.patch
 
-git am $MY_PATH/../patches/0001-FAB-11779-Move-goleveldb-version.patch
+#git am $MY_PATH/../patches/0001-FAB-11779-Move-goleveldb-version.patch
 
 # **Temporary** metrics collection
-git am $MY_PATH/../patches/0001-Ledger-metrics.patch
+#git am $MY_PATH/../patches/0001-Ledger-metrics.patch
