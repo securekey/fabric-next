9869efdf827399d969cf6be5033626843fe9eb2b
Emir Heidinger <emir.heidinger@securekey.com>
2018-09-14 13:50:14 -0400

    complete release fabric-next
    Change-Id: Ib27c782e7d59048d9f688c0197d12ff55e384ea1
    Signed-off-by: Emir Heidinger <emir.heidinger@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -44,8 +44,8 @@ git am $MY_PATH/../patches/0001-FAB-11247-Add-configuration-to-create-_global_ch
 #git am $MY_PATH/../patches/0001-FAB-11779-Move-goleveldb-version.patch
 
 # **Temporary** metrics collection
-#git am $MY_PATH/../patches/0001-Ledger-metrics.patch
+git am $MY_PATH/../patches/0001-Ledger-metrics.patch
 
 #apply patch for GREP11 (5151f212d3edd89fbabc12fbe702cecea0cb4b3a + local fixes)
-git am $MY_PATH/../patches/0001-GREP11-Remote-EP11-BCCSP.patch
-#git am $MY_PATH/../patches/0001-GREP11-Remote-EP11-BCCSP-metrics.patch
+#git am $MY_PATH/../patches/0001-GREP11-Remote-EP11-BCCSP.patch
+git am $MY_PATH/../patches/0001-GREP11-Remote-EP11-BCCSP-metrics.patch
