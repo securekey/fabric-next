bdc62b2c633ad6d0a4f226f121b7891163a6047e
Reza Mirzeinolabedin <reza.mirzeinolabedin@securekey.com>
2018-09-25 14:33:27 -0400

    [DEV-9670] Apply patch of change set #26477 to fabric-next
    - After moving the head to latest fabric release-1.2 (3cd00d1da6f216e9aaefcc5e0db0584c2cc46a5a)
      there is a conflict with ledger matrics patch.
      So, this change set contains new ledger metrics patch with resolved conflicts.
    
    Change-Id: I69a054aba38d5464b60004dba120349ef0c8ab2a
    Signed-off-by: Reza Mirzeinolabedin <reza.mirzeinolabedin@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -22,7 +22,7 @@ git clone https://gerrit.hyperledger.org/r/fabric
 cd fabric
 git config advice.detachedHead false
 # Tip of release-1.2 (September 6, 2018)
-git checkout 78a3a8dadf2a82c2e535bcacaeb95fbdd4fbafde
+git checkout 3cd00d1da6f216e9aaefcc5e0db0584c2cc46a5a
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
