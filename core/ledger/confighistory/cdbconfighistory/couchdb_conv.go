a29f3ada5cfcb1de43b34b28ceab20cefd94c7dc
Firas Qutishat <firas.qutishat@securekey.com>
2018-10-15 12:46:41 -0400

    [DEV-10003] move confighistory to couchdb
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>
    Change-Id: I91413f624c241da8b64ba1cefad51f3bf48a21c7

---
+++ core/ledger/confighistory/cdbconfighistory/couchdb_conv.go
@@ -0,0 +1,91 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package cdbconfighistory
+
+import (
+	"encoding/hex"
+	"encoding/json"
+
+	"github.com/hyperledger/fabric/core/ledger/util/couchdb"
+	"github.com/pkg/errors"
+)
+
+const (
+	idField                    = "_id"
+	binaryWrapper              = "valueBytes"
+	blockNumberField           = "block_number"
+	blockNumberCCNameIndexName = "by_block_number_cc_name"
+	blockNumberCCNameIndexDoc  = "indexBlockNumberCCName"
+	ccNameField                = "cc_name"
+)
+
+const blockNumberCCNameIndexDef = `
+	{
+		"index": {
+			"fields": ["` + blockNumberField + `","` + ccNameField + `"]
+		},
+		"name": "` + blockNumberCCNameIndexName + `",
+		"ddoc": "` + blockNumberCCNameIndexDoc + `",
+		"type": "json"
+	}`
+
+type jsonValue map[string]interface{}
+
+func (v jsonValue) toBytes() ([]byte, error) {
+	return json.Marshal(v)
+}
+
+func keyValueToCouchDoc(key []byte, value []byte, indices map[string]string) (*couchdb.CouchDoc, error) {
+	jsonMap := make(jsonValue)
+	k := hex.EncodeToString(key)
+	jsonMap[idField] = k
+
+	for key, val := range indices {
+		jsonMap[key] = val
+	}
+
+	jsonBytes, err := jsonMap.toBytes()
+	if err != nil {
+		return nil, err
+	}
+
+	couchDoc := couchdb.CouchDoc{JSONValue: jsonBytes}
+
+	attachment, err := valueToAttachment(value)
+	if err != nil {
+		return nil, err
+	}
+
+	attachments := append([]*couchdb.AttachmentInfo{}, attachment)
+	couchDoc.Attachments = attachments
+
+	return &couchDoc, nil
+}
+
+func valueToAttachment(v []byte) (*couchdb.AttachmentInfo, error) {
+	attachment := &couchdb.AttachmentInfo{}
+	attachment.AttachmentBytes = v
+	attachment.ContentType = "application/octet-stream"
+	attachment.Name = binaryWrapper
+
+	return attachment, nil
+}
+
+func couchAttachmentsValue(attachments []*couchdb.AttachmentInfo) ([]byte, error) {
+	var valueBytes []byte
+	// get binary data from attachment
+	for _, a := range attachments {
+		if a.Name == binaryWrapper {
+			valueBytes = a.AttachmentBytes
+		}
+	}
+
+	if len(valueBytes) == 0 {
+		return nil, errors.New("config history data is not within couchDB document")
+	}
+	return valueBytes, nil
+}
