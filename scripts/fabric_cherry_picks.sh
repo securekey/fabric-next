572e4bf5777e656b1f87aa15a3b4f1dd4e472f54
Troy Ronda <troy.ronda@securekey.com>
2018-10-19 15:33:21 -0400

    Update fabric-next cluster
    Change-Id: Ia5d663dc011455731fd0071e746bf20f2081ee91
    Signed-off-by: Troy Ronda <troy.ronda@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -22,7 +22,7 @@ git clone https://gerrit.securekey.com/fabric-next fabric
 cd fabric
 git config advice.detachedHead false
 # Tip of cluster (October 19, 2018)
-git checkout b379afbca47776195ed18e4e34a18ccf0adfdccd
+git checkout 7c271951107cdceeeeb5124d1096c699d327d304
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
