26ef7e5470a22e978922535a317c465b8a184edf
Firas Qutishat <firas.qutishat@securekey.com>
2018-10-22 18:05:20 -0400

    Make couchInstance singleton
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- core/ledger/util/couchdb/couchdbutil.go
+++ core/ledger/util/couchdb/couchdbutil.go
@@ -8,13 +8,15 @@ package couchdb
 import (
 	"encoding/hex"
 	"fmt"
-	"github.com/hyperledger/fabric/core/ledger/ledgerconfig"
 	"net"
 	"net/http"
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
+
+	"github.com/hyperledger/fabric/core/ledger/ledgerconfig"
 
 	"github.com/hyperledger/fabric/common/util"
 )
@@ -30,51 +32,54 @@ var maxLength = 238
 var chainNameAllowedLength = 50
 var namespaceNameAllowedLength = 50
 var collectionNameAllowedLength = 50
+var couchInstance *CouchInstance
+var once sync.Once
 
 //CreateCouchInstance creates a CouchDB instance
 func CreateCouchInstance(couchDBConnectURL, id, pw string, maxRetries,
 	maxRetriesOnStartup int, connectionTimeout time.Duration) (*CouchInstance, error) {
 
-	couchConf, err := CreateConnectionDefinition(couchDBConnectURL,
-		id, pw, maxRetries, maxRetriesOnStartup, connectionTimeout)
-	if err != nil {
-		logger.Errorf("Error during CouchDB CreateConnectionDefinition(): %s\n", err.Error())
-		return nil, err
-	}
+	once.Do(func() {
+		couchConf, err := CreateConnectionDefinition(couchDBConnectURL,
+			id, pw, maxRetries, maxRetriesOnStartup, connectionTimeout)
+		if err != nil {
+			panic(fmt.Sprintf("Error during CouchDB CreateConnectionDefinition(): %s\n", err.Error()))
+		}
 
-	// Create the HTTP transport.
-	// We override the default transport to enable configurable connection pooling.
-	transport, err := createHTTPTransport()
-	if err != nil {
-		return nil, err
-	}
+		// Create the HTTP transport.
+		// We override the default transport to enable configurable connection pooling.
+		transport, err := createHTTPTransport()
+		if err != nil {
+			panic(err.Error())
+		}
 
-	// Create the http client once
-	// Clients and Transports are safe for concurrent use by multiple goroutines
-	// and for efficiency should only be created once and re-used.
-	client := &http.Client{
-		Transport: transport,
-		Timeout: couchConf.RequestTimeout,
-	}
+		// Create the http client once
+		// Clients and Transports are safe for concurrent use by multiple goroutines
+		// and for efficiency should only be created once and re-used.
+		client := &http.Client{
+			Transport: transport,
+			Timeout:   couchConf.RequestTimeout,
+		}
 
-	//Create the CouchDB instance
-	couchInstance := &CouchInstance{conf: *couchConf, client: client}
+		//Create the CouchDB instance
+		couchInstance = &CouchInstance{conf: *couchConf, client: client}
 
-	connectInfo, retVal, verifyErr := couchInstance.VerifyCouchConfig()
-	if verifyErr != nil {
-		return nil, verifyErr
-	}
+		connectInfo, retVal, verifyErr := couchInstance.VerifyCouchConfig()
+		if verifyErr != nil {
+			panic(verifyErr)
+		}
 
-	//return an error if the http return value is not 200
-	if retVal.StatusCode != 200 {
-		return nil, fmt.Errorf("CouchDB connection error, expecting return code of 200, received %v", retVal.StatusCode)
-	}
+		//return an error if the http return value is not 200
+		if retVal.StatusCode != 200 {
+			panic(fmt.Sprintf("CouchDB connection error, expecting return code of 200, received %v", retVal.StatusCode))
+		}
 
-	//check the CouchDB version number, return an error if the version is not at least 2.0.0
-	errVersion := checkCouchDBVersion(connectInfo.Version)
-	if errVersion != nil {
-		return nil, errVersion
-	}
+		//check the CouchDB version number, return an error if the version is not at least 2.0.0
+		errVersion := checkCouchDBVersion(connectInfo.Version)
+		if errVersion != nil {
+			panic(errVersion)
+		}
+	})
 
 	return couchInstance, nil
 }
@@ -209,7 +214,6 @@ func joinSystemDBName(chainName, dbName string) string {
 	}
 	return systemDBName
 }
-
 
 // ConstructNamespaceDBName truncates db name to couchdb allowed length to
 // construct the namespaceDBName
