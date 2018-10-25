afa5339db12f550abd586d2d76f8e12fb3ce07ed
Troy Ronda <troy.ronda@securekey.com>
2018-10-25 11:17:26 -0400

    Move gossip message of blocks after commit
    Change-Id: Iafcbd0f2f0a5e05723bff04518e7d8cb7ad321d1
    Signed-off-by: Troy Ronda <troy.ronda@securekey.com>

--- core/deliverservice/blocksprovider/blocksprovider.go
+++ core/deliverservice/blocksprovider/blocksprovider.go
@@ -7,6 +7,7 @@ SPDX-License-Identifier: Apache-2.0
 package blocksprovider
 
 import (
+	"github.com/hyperledger/fabric/core/ledger/ledgerconfig"
 	"math"
 	"sync/atomic"
 	"time"
@@ -177,11 +178,8 @@ func (b *blocksProviderImpl) DeliverBlocks() {
 				continue
 			}
 
-			numberOfPeers := len(b.gossip.PeersOfChannel(gossipcommon.ChainID(b.chainID)))
 			// Create payload with a block received
 			payload := createPayload(blockNum, marshaledBlock)
-			// Use payload to create gossip message
-			gossipMsg := createGossipMsg(b.chainID, payload)
 
 			logger.Debugf("[%s] Adding payload to local buffer, blockNum = [%d]", b.chainID, blockNum)
 			// Add payload to local state payloads buffer
@@ -189,10 +187,17 @@ func (b *blocksProviderImpl) DeliverBlocks() {
 				logger.Warningf("Block [%d] received from ordering service wasn't added to payload buffer: %v", blockNum, err)
 			}
 
-			// Gossip messages with other nodes
-			logger.Debugf("[%s] Gossiping block [%d], peers number [%d]", b.chainID, blockNum, numberOfPeers)
-			if !b.isDone() {
-				b.gossip.Gossip(gossipMsg)
+			if !ledgerconfig.IsCommitter() {
+				numberOfPeers := len(b.gossip.PeersOfChannel(gossipcommon.ChainID(b.chainID)))
+
+				// Use payload to create gossip message
+				gossipMsg := createGossipMsg(b.chainID, payload)
+
+				// Gossip messages with other nodes
+				logger.Debugf("[%s] Gossiping block [%d], peers number [%d]", b.chainID, blockNum, numberOfPeers)
+				if !b.isDone() {
+					b.gossip.Gossip(gossipMsg)
+				}
 			}
 		default:
 			logger.Warningf("[%s] Received unknown: ", b.chainID, t)
