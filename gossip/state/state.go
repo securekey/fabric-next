825c12a72add268a38fdbf6411e92a5dff6627c1
Firas Qutishat <firas.qutishat@securekey.com>
2018-10-31 16:15:39 -0400

    Add getKVFromBlock
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>
    Change-Id: I34c520503e3e20a3b20faedf062456a3810b1cda

--- gossip/state/state.go
+++ gossip/state/state.go
@@ -17,19 +17,26 @@ import (
 	vsccErrors "github.com/hyperledger/fabric/common/errors"
 	"github.com/hyperledger/fabric/common/util/retry"
 	"github.com/hyperledger/fabric/core/ledger"
+	"github.com/hyperledger/fabric/core/ledger/kvledger"
+	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/rwsetutil"
 	"github.com/hyperledger/fabric/core/ledger/ledgerconfig"
 	ledgerUtil "github.com/hyperledger/fabric/core/ledger/util"
+	ledgerutil "github.com/hyperledger/fabric/core/ledger/util"
 	"github.com/hyperledger/fabric/gossip/api"
 	"github.com/hyperledger/fabric/gossip/comm"
 	common2 "github.com/hyperledger/fabric/gossip/common"
-	"github.com/hyperledger/fabric/gossip/discovery"
+
 	gossipimpl "github.com/hyperledger/fabric/gossip/gossip"
 	privdata2 "github.com/hyperledger/fabric/gossip/privdata"
 	"github.com/hyperledger/fabric/gossip/util"
+
+	"github.com/hyperledger/fabric/gossip/discovery"
 	"github.com/hyperledger/fabric/protos/common"
 	proto "github.com/hyperledger/fabric/protos/gossip"
 	"github.com/hyperledger/fabric/protos/ledger/rwset"
+	"github.com/hyperledger/fabric/protos/peer"
 	"github.com/hyperledger/fabric/protos/transientstore"
+	"github.com/hyperledger/fabric/protos/utils"
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 )
@@ -987,7 +994,90 @@ func (s *GossipStateProviderImpl) publishBlock(block *common.Block) error {
 		return errors.Wrapf(err, "error updating block number %d: %s", block.Header.Number, err)
 	}
 
+	_, err = getKVFromBlock(committedBlock)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func getKVFromBlock(block *common.Block) ([]kvledger.ValidatedTx, error) {
+	validatedTxs := make([]kvledger.ValidatedTx, 0)
+	for txIndex, envBytes := range block.Data.Data {
+		var chdr *common.ChannelHeader
+		var err error
+		txsFilter := ledgerutil.TxValidationFlags(block.Metadata.Metadata[common.BlockMetadataIndex_TRANSACTIONS_FILTER])
+		if env, err := utils.GetEnvelopeFromBlock(envBytes); err == nil {
+			if payload, err := utils.GetPayload(env); err == nil {
+				chdr, err = utils.UnmarshalChannelHeader(payload.Header.ChannelHeader)
+			}
+		}
+		if txsFilter.IsInvalid(txIndex) {
+			// Skipping invalid transaction
+			logger.Warningf("Channel [%s]: Block [%d] Transaction index [%d] TxId [%s]"+
+				" marked as invalid by committer will not add to cache. Reason code [%s]",
+				chdr.GetChannelId(), block.Header.Number, txIndex, chdr.GetTxId(),
+				txsFilter.Flag(txIndex).String())
+			continue
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		var txRWSet *rwsetutil.TxRwSet
+		txType := common.HeaderType(chdr.Type)
+		logger.Debugf("txType=%s", txType)
+		if txType == common.HeaderType_ENDORSER_TRANSACTION {
+			// extract actions from the envelope message
+			respPayload, err := utils.GetActionFromEnvelope(envBytes)
+			if err != nil {
+				logger.Warningf("Channel [%s]: Block [%d] Transaction index [%d] TxId [%s]"+
+					" GetActionFromEnvelope return error will not add to cache. Reason code [%s]",
+					chdr.GetChannelId(), block.Header.Number, txIndex, chdr.GetTxId(), err.Error())
+				continue
+			}
+			txRWSet = &rwsetutil.TxRwSet{}
+			if err = txRWSet.FromProtoBytes(respPayload.Results); err != nil {
+				txsFilter.SetFlag(txIndex, peer.TxValidationCode_INVALID_OTHER_REASON)
+				logger.Warningf("Channel [%s]: Block [%d] Transaction index [%d] TxId [%s]"+
+					" FromProtoBytes return error will not add to cache. Reason code [%s]",
+					chdr.GetChannelId(), block.Header.Number, txIndex, chdr.GetTxId(), err.Error())
+				continue
+			}
+		} else {
+			//TODO I'm not sure if we need this logic
+			//rwsetProto, err := processNonEndorserTx(env, chdr.TxId, txType, txmgr, !doMVCCValidation)
+			//if _, ok := err.(*customtx.InvalidTxError); ok {
+			//	txsFilter.SetFlag(txIndex, peer.TxValidationCode_INVALID_OTHER_REASON)
+			//	continue
+			//}
+			//if err != nil {
+			//	return nil, err
+			//}
+			//if rwsetProto != nil {
+			//	if txRWSet, err = rwsetutil.TxRwSetFromProtoMsg(rwsetProto); err != nil {
+			//		return nil, err
+			//	}
+			//}
+		}
+		if txRWSet != nil {
+			for _, nsRwSet := range txRWSet.NsRwSets {
+				if nsRwSet == nil {
+					continue
+				}
+				pubWriteset := nsRwSet.KvRwSet
+				if pubWriteset == nil {
+					continue
+				}
+
+				for _, kvwrite := range pubWriteset.Writes {
+					validatedTxs = append(validatedTxs, kvledger.ValidatedTx{Key: kvwrite.Key, Value: kvwrite.Value, BlockNum: block.Header.Number, IndexInBlock: txIndex})
+				}
+			}
+		}
+	}
+	return validatedTxs, nil
 }
 
 func (s *GossipStateProviderImpl) getCommittedBlock(block *common.Block) (*common.Block, error) {
