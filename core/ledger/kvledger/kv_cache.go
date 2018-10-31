197ba9d557443fe588d57ec47d2742ac85bef00e
Reza Mirzeinolabedin <reza.mirzeinolabedin@securekey.com>
2018-10-31 16:52:16 -0400

    [DEV-9984] A simple implementation of cache for key/values
    Change-Id: Ie0ae1a6ce381931e56ae3e6e61ea6c23d8d374eb
    Signed-off-by: Reza Mirzeinolabedin <reza.mirzeinolabedin@securekey.com>

---
+++ core/ledger/kvledger/kv_cache.go
@@ -0,0 +1,60 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package kvledger
+
+import (
+	"sync"
+
+	"github.com/golang/groupcache/lru"
+	"github.com/hyperledger/fabric/core/ledger/ledgerconfig"
+)
+
+type ValidatedTx struct {
+	Key          string
+	Value        []byte
+	BlockNum     uint64
+	IndexInBlock uint64
+}
+
+type KVCache struct {
+	ValidatedTxCache *lru.Cache
+	mutex            sync.RWMutex
+}
+
+func NewKVCache(
+	ledgerID string) *KVCache {
+	cacheSize := ledgerconfig.GetBlockCacheSize()
+
+	validatedTxCache := lru.New(cacheSize)
+	mtx := sync.RWMutex{}
+
+	cache := KVCache{
+		ValidatedTxCache: validatedTxCache,
+		mutex:            mtx,
+	}
+
+	return &cache
+}
+
+func (c *KVCache) Put(validatedTx ValidatedTx) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.ValidatedTxCache.Add(validatedTx.Key, validatedTx)
+}
+
+func (c *KVCache) Get(key string) (*ValidatedTx, bool) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	txn, ok := c.ValidatedTxCache.Get(key)
+	if !ok {
+		return nil, false
+	}
+
+	return txn.(*ValidatedTx), true
+}
