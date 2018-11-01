407dc3d9f396dfc91044d346e8b7977b9f04d0b5
Reza Mirzeinolabedin <reza.mirzeinolabedin@securekey.com>
2018-11-01 17:11:34 -0400

    - Some more modifications in kvcache - Populate the kvcache when block is received - Use the kvcache when there is a GetState() request for endorsment
    Change-Id: If70058a6461408148f56bb83436b9fe5b1bc411a
    Signed-off-by: Reza Mirzeinolabedin <reza.mirzeinolabedin@securekey.com>

---
+++ core/ledger/kvledger/txmgmt/statedb/kv_cache.go
@@ -0,0 +1,186 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package statedb
+
+import (
+	"sync"
+
+	"github.com/golang/groupcache/lru"
+	"github.com/hyperledger/fabric/core/ledger/ledgerconfig"
+	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/version"
+)
+
+type ValidatedTx struct {
+	Key          string
+	Value        []byte
+	BlockNum     uint64
+	IndexInBlock int
+}
+
+type ValidatedTxOp struct {
+	Namespace string
+	IsDeleted bool
+	ValidatedTx
+}
+
+type KVCache struct {
+	namespace        string
+	capacity         int
+	validatedTxCache *lru.Cache
+	mutex		     sync.Mutex
+	hit              uint64
+	miss             uint64
+}
+
+var (
+	kvCacheMap map[string]*KVCache
+	kvCacheMtx sync.Mutex
+)
+
+func InitKVCache() {
+	kvCacheMap = make(map[string]*KVCache)
+}
+
+func getKVCache(namespace string) (*KVCache, error) {
+	kvCache, found := kvCacheMap[namespace]
+	if !found {
+		kvCache = newKVCache(namespace)
+		kvCacheMap[namespace] = kvCache
+	}
+
+	return kvCache, nil
+}
+
+func GetKVCache(namespace string) (*KVCache, error) {
+	kvCacheMtx.Lock()
+	defer kvCacheMtx.Unlock()
+
+	return getKVCache(namespace)
+}
+
+func UpdateKVCache(validatedTxOps []ValidatedTxOp) {
+	kvCacheMtx.Lock()
+	defer kvCacheMtx.Unlock()
+
+	for _, validatedTxOp := range validatedTxOps {
+		kvCache, _ := getKVCache(validatedTxOp.Namespace)
+		if validatedTxOp.IsDeleted {
+			kvCache.Remove(validatedTxOp.ValidatedTx.Key, validatedTxOp.ValidatedTx.BlockNum, validatedTxOp.ValidatedTx.IndexInBlock)
+		} else {
+			newTx := validatedTxOp.ValidatedTx
+			kvCache.Put(&newTx)
+		}
+	}
+}
+
+func GetFromKVCache(namespace string, key string) (*VersionedValue, bool) {
+	kvCache, _ := GetKVCache(namespace)
+	if validatedTx, ok := kvCache.Get(key); ok {
+		versionedValue := &VersionedValue{
+			Value: validatedTx.Value,
+			Version: &version.Height{
+				BlockNum: validatedTx.BlockNum,
+				TxNum:    uint64(validatedTx.IndexInBlock),
+			},
+		}
+		return versionedValue, true
+	}
+	return nil, false
+}
+
+func newKVCache(
+	namespace string) *KVCache {
+	cacheSize := ledgerconfig.GetKVCacheSize()
+
+	validatedTxCache := lru.New(cacheSize)
+
+	cache := KVCache{
+		namespace:        namespace,
+		capacity:         cacheSize,
+		validatedTxCache: validatedTxCache,
+	}
+
+	return &cache
+}
+
+func (c *KVCache) get(key string) (*ValidatedTx, bool) {
+	txn, ok := c.validatedTxCache.Get(key)
+
+	if ok {
+		return txn.(*ValidatedTx), ok
+	}
+
+	return nil, false
+}
+
+func (c *KVCache) Put(validatedTx *ValidatedTx) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	exitingKeyVal, found := c.get(validatedTx.Key)
+	// Add to the cache if the exisiting version is older
+	if (found && exitingKeyVal.BlockNum < validatedTx.BlockNum) ||
+		(found && exitingKeyVal.BlockNum == validatedTx.BlockNum && exitingKeyVal.IndexInBlock < validatedTx.IndexInBlock) || !found {
+		c.validatedTxCache.Add(validatedTx.Key, validatedTx)
+	}
+}
+
+func (c *KVCache) Get(key string) (*ValidatedTx, bool) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	txn, ok := c.get(key)
+	if !ok {
+		c.miss++
+		return nil, false
+	}
+
+	c.hit++
+	return txn, true
+}
+
+func (c *KVCache) Size() int {
+	return c.validatedTxCache.Len()
+}
+
+func (c *KVCache) Capacity() int {
+	return c.capacity
+}
+
+func (c *KVCache) MustRemove(key string) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.validatedTxCache.Remove(key)
+}
+
+// Remove from the cache if the blockNum and indexInBlock are bigger than the corresponding values in the cache
+func (c *KVCache) Remove(key string, blockNum uint64, indexInBlock int) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	exitingKeyVal, found := c.get(key)
+	// Remove from the cache if the existing version is older
+	if (found && exitingKeyVal.BlockNum < blockNum) ||
+		(found && exitingKeyVal.BlockNum == blockNum && exitingKeyVal.IndexInBlock < indexInBlock){
+		c.validatedTxCache.Remove(key)
+	}
+}
+
+func (c *KVCache) Clear() {
+	c.miss = 0
+	c.hit = 0
+	c.validatedTxCache.Clear()
+}
+
+func (c *KVCache) Hit() uint64 {
+	return c.hit
+}
+
+func (c *KVCache) Miss() uint64 {
+	return c.miss
+}
