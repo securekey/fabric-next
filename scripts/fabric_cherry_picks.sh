db1d8c6727b64b901ed7c364ed00eba1868fb320
Firas Qutishat <firas.qutishat@securekey.com>
2018-11-07 16:14:05 -0500

    Update fabric-next cluster
    Change-Id: I28c6e09c0fbba7e111818e37336426a52a4f87a2
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -22,7 +22,7 @@ git clone https://gerrit.securekey.com/fabric-next fabric
 cd fabric
 git config advice.detachedHead false
 # Tip of cluster (November 7, 2018)
-git checkout 3e5dbf6891a2c621df07b3732d67b67a7f0fece3
+git checkout cf923f63d3f1cf6a3ef58b8b1d3341af912a979f
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
