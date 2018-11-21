ae0f0534fe7865c635c2075959b985623690d025
Firas Qutishat <firas.qutishat@securekey.com>
2018-11-21 16:48:21 -0500

    Update fabric-next cluster 0431ca70c4901b8cd46cc2e3e04122d04f544ee2
    Change-Id: Ibfa227b3f0e848b891797deaf760332a8609d2ac
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -22,30 +22,9 @@ git clone https://gerrit.securekey.com/fabric-next fabric
 cd fabric
 git config advice.detachedHead false
 # Tip of cluster (November 21, 2018)
-git checkout 5fa507d2dd2093e9c84187ee7693cd7477d84cb3
+git checkout 0431ca70c4901b8cd46cc2e3e04122d04f544ee2
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
 git config user.name "jenkins"
 git config user.email jenkins@jenkins.com
-
-# Cherry pick [FAB-11777] Expose LevelDB configuration
-git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/29/25929/4 && git cherry-pick FETCH_HEAD
-
-# Cherry pick [FAB-11907] Data races in deliver client
-git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/70/26170/4 && git cherry-pick FETCH_HEAD
-
-# [FAB-11247] Add configuration to create _global_changes
-git am $MY_PATH/../patches/0001-FAB-11247-Add-configuration-to-create-_global_change.patch
-
-# [FAB-8622] Reduce exclusive lock duration during commit
-#git am $MY_PATH/../patches/0001-FAB-8622-Red.-exclusive-lock-dur.-during-commit.patch
-
-#git am $MY_PATH/../patches/0001-FAB-11779-Move-goleveldb-version.patch
-
-# **Temporary** metrics collection
-git am -3 $MY_PATH/../patches/0001-Ledger-metrics.patch
-
-#apply patch for GREP11 (5151f212d3edd89fbabc12fbe702cecea0cb4b3a + local fixes)
-#git am $MY_PATH/../patches/0001-GREP11-Remote-EP11-BCCSP.patch
-git am $MY_PATH/../patches/0001-GREP11-Remote-EP11-BCCSP-metrics.patch
