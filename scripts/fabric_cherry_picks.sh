b3827cb1a3b2641419eaa612002971d26e34e9c9
Sudesh Shetty <sudesh.shetty@securekey.com>
2018-11-23 16:47:28 -0500

    [DEV-10585] Applying background commit update
    Change-Id: I51e61978684725046168ad30b649b2964f4dfa82
    Signed-off-by: Sudesh Shetty <sudesh.shetty@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -22,7 +22,7 @@ git clone https://gerrit.securekey.com/fabric-next fabric
 cd fabric
 git config advice.detachedHead false
 # Tip of cluster (November 23, 2018)
-git checkout bac9b2b207ceae882c17c25bdfc1f545a606761e
+git checkout 6d37d67a2bfb62a7cbe7a354b203ba388755ab05
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
