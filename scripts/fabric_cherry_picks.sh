c6fb4818680bb4fbe6fc3aa1a60557533da2f58a
Firas Qutishat <firas.qutishat@securekey.com>
2018-12-12 13:46:56 -0500

    Apply Add metric for num of txn
    Change-Id: I77a4de5d7674bc597064f65b0a17a26f372c4aa6
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -21,8 +21,8 @@ cd $GOPATH/src/github.com/hyperledger/
 git clone https://gerrit.securekey.com/fabric-next fabric
 cd fabric
 git config advice.detachedHead false
-# Tip of cluster (December 11, 2018)
-git checkout 35ae3404236fc6ef46e2a5f1c741565bb4ab1706
+# Tip of cluster (December 12, 2018)
+git checkout 84ba20cf0809dd20ec37da9764315ce597bbef3d
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
