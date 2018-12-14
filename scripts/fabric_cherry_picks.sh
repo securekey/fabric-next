94e7672cc954c80e75a7143b62d3598bb4ea6cda
Firas Qutishat <firas.qutishat@securekey.com>
2018-12-13 19:16:57 -0500

    Apply [DEV-11199] Intra-block validation in parallel with MVCC
    Change-Id: Ib3e6f542f83d67a970c4b9df46428378162984cb
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -22,7 +22,7 @@ git clone https://gerrit.securekey.com/fabric-next fabric
 cd fabric
 git config advice.detachedHead false
 # Tip of cluster (December 13, 2018)
-git checkout 70e984f47e8a53eb2e6d857a19195333e1611111
+git checkout 8e3872a84a730daa4414e9ea624c13f503f46306
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
