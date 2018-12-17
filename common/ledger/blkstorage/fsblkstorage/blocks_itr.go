91fc87c56bc938a9a7ddef5745804269de7c8865
talwinder.kaur <Talwinder.Kaur@securekey.com>
2018-12-16 20:16:36 -0500

    [DEV-11242] Resolve Merge conflict common/ledger/blkstorage/
    Signed-off-by: talwinder.kaur <Talwinder.Kaur@securekey.com>
    Change-Id: Ie6c91540f211c7975fb256c5584a5e2ddf308d01

--- common/ledger/blkstorage/fsblkstorage/blocks_itr.go
+++ common/ledger/blkstorage/fsblkstorage/blocks_itr.go
@@ -1,12 +1,9 @@
 /*
 Copyright IBM Corp. 2016 All Rights Reserved.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
 		 http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,74 +12,40 @@ limitations under the License.
 */
 /*
 Copyright SecureKey Technologies Inc. All Rights Reserved.
-
 SPDX-License-Identifier: Apache-2.0
 */
-
 package fsblkstorage
-
 import (
 	"context"
 	"github.com/hyperledger/fabric/common/ledger"
 	"sync"
 )
-
 // blocksItr - an iterator for iterating over a sequence of blocks
 type blocksItr struct {
 	mgr                  *blockfileMgr
 	maxBlockNumAvailable uint64
 	blockNumToRetrieve   uint64
 	stream               *blockStream
-	closeMarker          bool
-	closeMarkerLock      *sync.Mutex
+	streamMtx            sync.Mutex
+	ctx                  context.Context
+	cancel               context.CancelFunc
 }
-
 func newBlockItr(mgr *blockfileMgr, startBlockNum uint64) *blocksItr {
-	mgr.cpInfoCond.L.Lock()
-	defer mgr.cpInfoCond.L.Unlock()
-	return &blocksItr{mgr, mgr.cpInfo.lastBlockNumber, startBlockNum, nil, false, &sync.Mutex{}}
+	ctx, cancel := context.WithCancel(context.Background())
+	return &blocksItr{mgr, mgr.lastBlockNumber(), startBlockNum, nil, sync.Mutex{}, ctx, cancel}
 }
-
-func (itr *blocksItr) waitForBlock(blockNum uint64) uint64 {
-	itr.mgr.cpInfoCond.L.Lock()
-	defer itr.mgr.cpInfoCond.L.Unlock()
-	for itr.mgr.cpInfo.lastBlockNumber < blockNum && !itr.shouldClose() {
-		logger.Debugf("Going to wait for newer blocks. maxAvailaBlockNumber=[%d], waitForBlockNum=[%d]",
-			itr.mgr.cpInfo.lastBlockNumber, blockNum)
-		itr.mgr.cpInfoCond.Wait()
-		logger.Debugf("Came out of wait. maxAvailaBlockNumber=[%d]", itr.mgr.cpInfo.lastBlockNumber)
-	}
-	return itr.mgr.cpInfo.lastBlockNumber
-}
-
-func (itr *blocksItr) initStream() error {
-	var lp *fileLocPointer
-	var err error
-	if lp, err = itr.mgr.index.getBlockLocByBlockNum(itr.blockNumToRetrieve); err != nil {
-		return err
-	}
-	if itr.stream, err = newBlockStream(itr.mgr.rootDir, lp.fileSuffixNum, int64(lp.offset), -1); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (itr *blocksItr) shouldClose() bool {
-	itr.closeMarkerLock.Lock()
-	defer itr.closeMarkerLock.Unlock()
-	return itr.closeMarker
-}
-
 // Next moves the cursor to next block and returns true iff the iterator is not exhausted
 func (itr *blocksItr) Next() (ledger.QueryResult, error) {
 	if itr.maxBlockNumAvailable < itr.blockNumToRetrieve {
-		itr.maxBlockNumAvailable = itr.waitForBlock(itr.blockNumToRetrieve)
+		itr.maxBlockNumAvailable = itr.mgr.waitForBlock(itr.ctx, itr.blockNumToRetrieve)
 	}
-	itr.closeMarkerLock.Lock()
-	defer itr.closeMarkerLock.Unlock()
-	if itr.closeMarker {
+	select {
+	case <-itr.ctx.Done():
 		return nil, nil
+	default:
 	}
+	itr.streamMtx.Lock()
+	defer itr.streamMtx.Unlock()
 	if itr.stream == nil {
 		logger.Debugf("Initializing block stream for iterator. itr.maxBlockNumAvailable=%d", itr.maxBlockNumAvailable)
 		if err := itr.initStream(); err != nil {
@@ -96,7 +59,6 @@ func (itr *blocksItr) Next() (ledger.QueryResult, error) {
 	itr.blockNumToRetrieve++
 	return deserializeBlock(nextBlockBytes)
 }
-
 func (itr *blocksItr) initStream() error {
 	var lp *fileLocPointer
 	var err error
@@ -108,11 +70,9 @@ func (itr *blocksItr) initStream() error {
 	}
 	return nil
 }
-
 // Close releases any resources held by the iterator
 func (itr *blocksItr) Close() {
 	itr.cancel()
-
 	itr.streamMtx.Lock()
 	if itr.stream != nil {
 		itr.stream.close()
