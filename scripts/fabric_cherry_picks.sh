54d7ec676659a05e192521f34caf7c16c960735d
Sandra Vrtikapa <sandra.vrtikapa@securekey.com>
2018-12-18 10:29:40 -0500

    [DEV-11134] Add support for lte in purge selector
    Signed-off-by: Sandra Vrtikapa <sandra.vrtikapa@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -21,8 +21,8 @@ cd $GOPATH/src/github.com/hyperledger/
 git clone https://gerrit.securekey.com/fabric-next fabric
 cd fabric
 git config advice.detachedHead false
-# Tip of cluster (December 17 - 4:48 pm, 2018)
-git checkout 687070b2a1bd7fdcac6563d1242d946b9d5766b3
+# Tip of cluster (December 18, 2018)
+git checkout 132403ddc2e6201ba13ec5b56a1bf4cefadb3504
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
