ed3033776abc194f243143c10b2567ae87838bd0
Firas Qutishat <firas.qutishat@securekey.com>
2018-12-20 12:21:41 -0500

    Apply verify lastCommittedBlock for committer only
    Change-Id: I372e0e528af029cdbd9e7a2e5bef095e2577709c
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -22,7 +22,7 @@ git clone https://gerrit.securekey.com/fabric-next fabric
 cd fabric
 git config advice.detachedHead false
 # Tip of cluster (December 19, 2018)
-git checkout 8e3872a84a730daa4414e9ea624c13f503f46306
+git checkout 687070b2a1bd7fdcac6563d1242d946b9d5766b3
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
