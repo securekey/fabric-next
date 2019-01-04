9ccea846d9223d0b570fb032523497713d5362e3
Bob Stasyszyn <Bob.Stasyszyn@securekey.com>
2019-01-04 09:48:50 -0500

    Apply [DEV-11320] Fix for missing pvt data
    Change-Id: Ibaf73d508d4f0966384f27efe1949b7226ec04f7
    Signed-off-by: Bob Stasyszyn <Bob.Stasyszyn@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -21,8 +21,8 @@ cd $GOPATH/src/github.com/hyperledger/
 git clone https://gerrit.securekey.com/fabric-next fabric
 cd fabric
 git config advice.detachedHead false
-# Tip of cluster (January 03, 2019)
-git checkout 5ddc41a4aa093324151b6f999f431ce9639bd136
+# Tip of cluster (January 04, 2019)
+git checkout 018ec48906e17fa289cd7dcf875cd3ba0604d4b4
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
