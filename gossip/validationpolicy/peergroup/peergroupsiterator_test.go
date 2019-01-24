cea4d4d62ef5ee27cb1c3f9418571dda435e9887
Bob Stasyszyn <Bob.Stasyszyn@securekey.com>
2019-01-24 17:41:36 -0500

    [DEV-11271] Cross-Org Validation
    Change-Id: Ic48e61d73f13c69284d0c06670a01eb59f51f413
    Signed-off-by: Bob Stasyszyn <Bob.Stasyszyn@securekey.com>

---
+++ gossip/validationpolicy/peergroup/peergroupsiterator_test.go
@@ -0,0 +1,35 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package peergroup
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	pg1 = PeerGroup{}
+	pg2 = PeerGroup{}
+	pg3 = PeerGroup{}
+	pg4 = PeerGroup{}
+)
+
+func TestIterator(t *testing.T) {
+
+	peerGroups := PeerGroups{pg1, pg2, pg3, pg4}
+
+	startingIndex := 2
+
+	it := NewIterator(peerGroups, startingIndex)
+
+	assert.Equal(t, pg3, it.Next())
+	assert.Equal(t, pg4, it.Next())
+	assert.Equal(t, pg1, it.Next())
+	assert.Equal(t, pg2, it.Next())
+	assert.Nil(t, it.Next())
+}
