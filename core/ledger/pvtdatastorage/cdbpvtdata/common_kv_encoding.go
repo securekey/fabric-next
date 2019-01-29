4da62d4d53da946894f962e451d6c8c644626ad9
Firas Qutishat <firas.qutishat@securekey.com>
2019-01-29 10:17:10 -0500

    Fix pvtdatastorage 	after Fabric 1.4 rebase
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>
    Change-Id: I8885a61e6f5524b81f04cd6e821e2297e525ee18
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- core/ledger/pvtdatastorage/cdbpvtdata/common_kv_encoding.go
+++ core/ledger/pvtdatastorage/cdbpvtdata/common_kv_encoding.go
@@ -1,26 +1,29 @@
 /*
 Copyright IBM Corp. All Rights Reserved.
-
 SPDX-License-Identifier: Apache-2.0
 */
-
 package cdbpvtdata
 
 import (
 	"bytes"
-
-	"github.com/hyperledger/fabric/core/ledger/pvtdatastorage/pvtmetadata"
+	math "math"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/version"
+	"github.com/hyperledger/fabric/core/ledger/util"
 	"github.com/hyperledger/fabric/protos/ledger/rwset"
+	"github.com/pkg/errors"
+	"github.com/willf/bitset"
 )
 
 var (
-	pendingCommitKey    = []byte{0}
-	lastCommittedBlkkey = []byte{1}
-	pvtDataKeyPrefix    = []byte{2}
-	expiryKeyPrefix     = []byte{3}
+	pendingCommitKey               = []byte{0}
+	lastCommittedBlkkey            = []byte{1}
+	pvtDataKeyPrefix               = []byte{2}
+	expiryKeyPrefix                = []byte{3}
+	eligibleMissingDataKeyPrefix   = []byte{4}
+	ineligibleMissingDataKeyPrefix = []byte{5}
+	collElgKeyPrefix               = []byte{6}
 
 	nilByte    = byte(0)
 	emptyValue = []byte{}
@@ -32,26 +35,25 @@ func encodeDataKey(key *dataKey) []byte {
 	dataKeyBytes = append(dataKeyBytes, nilByte)
 	return append(dataKeyBytes, []byte(key.coll)...)
 }
-
 func encodeDataValue(collData *rwset.CollectionPvtReadWriteSet) ([]byte, error) {
 	return proto.Marshal(collData)
 }
-
 func encodeExpiryKey(expiryKey *expiryKey) []byte {
 	// reusing version encoding scheme here
 	return append(expiryKeyPrefix, version.NewHeight(expiryKey.expiringBlk, expiryKey.committingBlk).ToBytes()...)
 }
-
-func encodeExpiryValue(expiryData *pvtmetadata.ExpiryData) ([]byte, error) {
+func encodeExpiryValue(expiryData *ExpiryData) ([]byte, error) {
 	return proto.Marshal(expiryData)
 }
-
-func decodeExpiryValue(expiryValueBytes []byte) (*pvtmetadata.ExpiryData, error) {
-	expiryData := &pvtmetadata.ExpiryData{}
+func decodeExpiryKey(expiryKeyBytes []byte) *expiryKey {
+	height, _ := version.NewHeightFromBytes(expiryKeyBytes[1:])
+	return &expiryKey{expiringBlk: height.BlockNum, committingBlk: height.TxNum}
+}
+func decodeExpiryValue(expiryValueBytes []byte) (*ExpiryData, error) {
+	expiryData := &ExpiryData{}
 	err := proto.Unmarshal(expiryValueBytes, expiryData)
 	return expiryData, err
 }
-
 func decodeDatakey(datakeyBytes []byte) *dataKey {
 	v, n := version.NewHeightFromBytes(datakeyBytes[1:])
 	blkNum := v.BlockNum
@@ -60,16 +62,106 @@ func decodeDatakey(datakeyBytes []byte) *dataKey {
 	nilByteIndex := bytes.IndexByte(remainingBytes, nilByte)
 	ns := string(remainingBytes[:nilByteIndex])
 	coll := string(remainingBytes[nilByteIndex+1:])
-	return &dataKey{blkNum: blkNum, txNum: tranNum, ns: ns, coll: coll}
+	return &dataKey{nsCollBlk: nsCollBlk{ns, coll, blkNum}, txNum: tranNum}
 }
-
 func decodeDataValue(datavalueBytes []byte) (*rwset.CollectionPvtReadWriteSet, error) {
 	collPvtdata := &rwset.CollectionPvtReadWriteSet{}
 	err := proto.Unmarshal(datavalueBytes, collPvtdata)
 	return collPvtdata, err
 }
 
-func decodeExpiryKey(expiryKeyBytes []byte) *expiryKey {
-	height, _ := version.NewHeightFromBytes(expiryKeyBytes[1:])
-	return &expiryKey{expiringBlk: height.BlockNum, committingBlk: height.TxNum}
+func createRangeScanKeysForEligibleMissingDataEntries(blkNum uint64) (startKey, endKey []byte) {
+	startKey = append(eligibleMissingDataKeyPrefix, util.EncodeReverseOrderVarUint64(blkNum)...)
+	endKey = append(eligibleMissingDataKeyPrefix, util.EncodeReverseOrderVarUint64(0)...)
+
+	return startKey, endKey
+}
+
+func decodeMissingDataKey(keyBytes []byte) *missingDataKey {
+	key := &missingDataKey{nsCollBlk: nsCollBlk{}}
+	if keyBytes[0] == eligibleMissingDataKeyPrefix[0] {
+		blkNum, numBytesConsumed := util.DecodeReverseOrderVarUint64(keyBytes[1:])
+
+		splittedKey := bytes.Split(keyBytes[numBytesConsumed+1:], []byte{nilByte})
+		key.ns = string(splittedKey[0])
+		key.coll = string(splittedKey[1])
+		key.blkNum = blkNum
+		key.isEligible = true
+		return key
+	}
+
+	splittedKey := bytes.SplitN(keyBytes[1:], []byte{nilByte}, 3) //encoded bytes for blknum may contain empty bytes
+	key.ns = string(splittedKey[0])
+	key.coll = string(splittedKey[1])
+	key.blkNum, _ = util.DecodeReverseOrderVarUint64(splittedKey[2])
+	key.isEligible = false
+	return key
+}
+
+func encodeMissingDataKey(key *missingDataKey) []byte {
+	if key.isEligible {
+		keyBytes := append(eligibleMissingDataKeyPrefix, util.EncodeReverseOrderVarUint64(key.blkNum)...)
+		keyBytes = append(keyBytes, []byte(key.ns)...)
+		keyBytes = append(keyBytes, nilByte)
+		return append(keyBytes, []byte(key.coll)...)
+	}
+	keyBytes := append(ineligibleMissingDataKeyPrefix, []byte(key.ns)...)
+	keyBytes = append(keyBytes, nilByte)
+	keyBytes = append(keyBytes, []byte(key.coll)...)
+	keyBytes = append(keyBytes, nilByte)
+	return append(keyBytes, []byte(util.EncodeReverseOrderVarUint64(key.blkNum))...)
+}
+
+func decodeMissingDataValue(bitmapBytes []byte) (*bitset.BitSet, error) {
+	bitmap := &bitset.BitSet{}
+	if err := bitmap.UnmarshalBinary(bitmapBytes); err != nil {
+		return nil, err
+	}
+	return bitmap, nil
+}
+
+func encodeCollElgKey(blkNum uint64) []byte {
+	return append(collElgKeyPrefix, util.EncodeReverseOrderVarUint64(blkNum)...)
+}
+
+func encodeCollElgVal(m *CollElgInfo) ([]byte, error) {
+	return proto.Marshal(m)
+}
+
+func createRangeScanKeysForCollElg() (startKey, endKey []byte) {
+	return encodeCollElgKey(math.MaxUint64),
+		encodeCollElgKey(0)
+}
+
+func decodeCollElgKey(b []byte) uint64 {
+	blkNum, _ := util.DecodeReverseOrderVarUint64(b[1:])
+	return blkNum
+}
+
+func decodeCollElgVal(b []byte) (*CollElgInfo, error) {
+	m := &CollElgInfo{}
+	if err := proto.Unmarshal(b, m); err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return m, nil
+}
+
+func createRangeScanKeysForIneligibleMissingData(maxBlkNum uint64, ns, coll string) (startKey, endKey []byte) {
+	startKey = encodeMissingDataKey(
+		&missingDataKey{
+			nsCollBlk:  nsCollBlk{ns: ns, coll: coll, blkNum: maxBlkNum},
+			isEligible: false,
+		},
+	)
+	endKey = encodeMissingDataKey(
+		&missingDataKey{
+			nsCollBlk:  nsCollBlk{ns: ns, coll: coll, blkNum: 0},
+			isEligible: false,
+		},
+	)
+	return
+}
+
+func encodeMissingDataValue(bitmap *bitset.BitSet) ([]byte, error) {
+	return bitmap.MarshalBinary()
 }
