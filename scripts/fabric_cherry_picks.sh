9e4a7d3707d180401cb974bc4ed0b7f41e10b2c2
Rolson Quadras <rolson.quadras@securekey.com>
2019-04-12 12:37:00 -0400

    [DEV-13206] fabric-next release based on fabric v1.4.1
    Change-Id: Ib3580db007a8cb76275309e142aa442879fd3cc9
    Signed-off-by: Rolson Quadras <rolson.quadras@securekey.com>

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -21,8 +21,8 @@ cd $GOPATH/src/github.com/hyperledger/
 git clone https://gerrit.hyperledger.org/r/fabric
 cd fabric
 git config advice.detachedHead false
-# Fabric release-1.4 (Jan 24, 2019)
-git checkout 1fdb6917c71bc4bfbd2cddde9a6d285a2a8ee390
+# Fabric release-1.4.1 (Apr 11, 2019) - https://github.com/hyperledger/fabric/releases/tag/v1.4.1
+git checkout 87074a73f3f6fcdff7e82707c9b476596d030d04
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
@@ -36,28 +36,4 @@ git am $MY_PATH/../patches/0001-GREP11-Remote-EP11-BCCSP.patch
 
 # [FAB-14646] Update dependency github.com/opencontainers/runc
 git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/94/30094/1 && git cherry-pick FETCH_HEAD
-
-#[FAB-12982] & [FAB-13534] cherry picks are required to [FAB-12914] gossip private data metrics
-
-#[FAB-12982] spray pvt across maximum eligible orgs
-git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/15/27815/10 && git cherry-pick FETCH_HEAD
-#[FAB-13534] cache pushAckTimeout in pvt data
-git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/26/28526/2 && git cherry-pick FETCH_HEAD
-
-#cherry pick metrics
-
-#[FAB-12916] gossip state metrics
-git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/92/28692/10 && git cherry-pick FETCH_HEAD
-
-#[FAB-12915] gossip leader election metrics
-git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/79/28779/12 && git cherry-pick FETCH_HEAD
-
-#[FAB-12917] gossip comm metrics
-git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/84/28784/17 && git cherry-pick FETCH_HEAD
-
-#[FAB-12918] gossip channel membership metrics
-git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/51/28851/17 && git cherry-pick FETCH_HEAD
-
-#[FAB-12914] gossip private data metrics
-git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/38/28938/7 && git cherry-pick FETCH_HEAD
 
