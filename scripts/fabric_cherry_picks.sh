23229a8a142ab89988db3965da2b1bcd64067396
Firas Qutishat <firas.qutishat@securekey.com>
2019-08-28 12:31:51 -0400

    Upgrade to fabric v1.4.3
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>
    Change-Id: I13d40843a541e64c1e10d98f5d8acd0ca2d695e0

--- scripts/fabric_cherry_picks.sh
+++ scripts/fabric_cherry_picks.sh
@@ -21,8 +21,8 @@ cd $GOPATH/src/github.com/hyperledger/
 git clone https://gerrit.hyperledger.org/r/fabric
 cd fabric
 git config advice.detachedHead false
-# https://github.com/hyperledger/fabric/releases/tag/v1.4.2
-git checkout v1.4.2
+# https://github.com/hyperledger/fabric/releases/tag/v1.4.3
+git checkout v1.4.3
 
 cd $GOPATH/src/github.com/hyperledger/fabric
 
@@ -39,7 +39,7 @@ git fetch https://gerrit.hyperledger.org/r/fabric refs/changes/94/30094/1 && git
 # step 1 apply gossip protos extensions to match trustbloc/fabric-peer-ext package names dependencies
 git am $MY_PATH/../patches/0001-gossip-protos-extensions-refactoring.patch
 # step 2 apply transient data changes from trustbloc version of Fabric
-git am $MY_PATH/../patches/0002-Backport-Transient-Data.patch
+git am $MY_PATH/../patches/0001-Backport-Transient-Data.patch
 # step 3 apply transient data fix BLOC-1741 Fix for deadlock
 git am $MY_PATH/../patches/0001-BLOC-1741-Fix-deadlock-when-retrieving-transient-dat.patch
 # step 4 apply transient data fix BLOC-1814 Missing transient data when mutiple keys in write-set
@@ -48,5 +48,5 @@ git am $MY_PATH/../patches/0001-BLOC-1814-Missing-transient-data-if-multiple-key
 git am $MY_PATH/../patches/0001-BLOC-1827-Prevent-modification-of-collection-type-du.patch
 # step 6 apply concurrent map write fix BLOC-1833 Concurrent writes to roles map
 git am $MY_PATH/../patches/0001-BLOC-1833-Concurrent-writes-to-roles-map.patch
-# step 6 apply patch for BLOC-1836 Performance improvement for pulling private data
+# step 7 apply patch for BLOC-1836 Performance improvement for pulling private data
 git am $MY_PATH/../patches/0001-BLOC-1836-Performance-improvement-for-pulling-privat.patch
