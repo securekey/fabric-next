d7886de64289d6e3ad61db16079a8c0e8fcfc046
Ahmed Sajid <ahmed.sajid@securekey.com>
2019-10-22 22:19:08 -0400

    Added fuctionality to use SK baseimages for thirdparty images
    Signed-off-by: Ahmed Sajid <ahmed.sajid@securekey.com>
    Change-Id: I5e31318159f29b7ecc8d3243fdac561f126663dd

---
+++ scripts/fabric_baseimage.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright SecureKey Technologies Inc. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+set -e
+
+# NEEDED for GREP11 patch
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
+
+mkdir -p $GOPATH/src/github.com/hyperledger/
+cd $GOPATH/src/github.com/hyperledger/
+git clone https://gerrit.hyperledger.org/r/fabric-baseimage
+cd fabric-baseimage
+git config advice.detachedHead false
+# https://github.com/hyperledger/fabric-baseimage/releases/tag/v0.4.5
+git checkout v0.4.15
+
+cd $GOPATH/src/github.com/hyperledger/fabric-baseimage
+
+git config user.name "jenkins"
+git config user.email jenkins@jenkins.com
