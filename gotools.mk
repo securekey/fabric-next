510246ea382b4e41ef548340ed212d9fac96e787
Bob Stasyszyn <Bob.Stasyszyn@securekey.com>
2020-08-07 09:02:04 -0400

    Add standby role to peer
    Added a role 'standby' which, if set, prevents the peer from publishing it's channel membership. This allows the peer to be invisible to clients so that it doesn't get selected for endorsements, but at the same time allow it to commit blocks.
    
    Signed-off-by: Bob Stasyszyn <Bob.Stasyszyn@securekey.com>
    Change-Id: I9f52c61300d6dba203853b78efdc72de1eaefcb2

--- gotools.mk
+++ gotools.mk
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-GOTOOLS = counterfeiter dep golint goimports protoc-gen-go ginkgo gocov gocov-xml misspell mockery manifest-tool
+GOTOOLS = counterfeiter dep golint goimports protoc-gen-go ginkgo gocov gocov-xml misspell manifest-tool
 BUILD_DIR ?= .build
 GOTOOLS_GOPATH ?= $(BUILD_DIR)/gotools
 GOTOOLS_BINDIR ?= $(GOPATH)/bin
